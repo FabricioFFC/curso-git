3a6c2ea3d93040e2871668daf470d4191d8d0b92
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2011-03-19 13:06:25 -0300

    arrumado nome do método initialize

--- people.rb
+++ people.rb
@@ -1,19 +1,20 @@
 class Person
   attr_accessor :name, :last, :age, :sex
-  
-  def intialize(name, last, age, sex)
+
+  def initialize(name, last, age, sex)
     @name, @last, @age, @sex = name, last, age, sex
   end
-  
+
   def diga_ola
-    if (age < 2) 
+    if (age < 2)
       "cuti, cuti"
     else
       "Olá #{full_name} você é #{sex}!"
     end
   end
-  
+
   def full_name
     "#{name} #{last}"
   end
 end
+
